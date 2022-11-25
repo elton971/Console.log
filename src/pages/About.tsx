7bbb86528119752ca41e30900b7eea0c58e51477
Elton Cavele <82831720+elton971@users.noreply.github.com>
2022-11-25 09:34:43 +0200

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,7 +2,6 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import { SvgIcon } from '@mui/material';
-import {NavBar} from "../components/NavBar";
 import AppBarComponent from "../components/AppBar";
 
 export const About =()=>{
